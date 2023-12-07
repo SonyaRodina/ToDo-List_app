cf32b848b0d9051e995efa43113646d8fba2d1af
SonyaRodina <sonya-rodina1@mail.ru>
2023-12-07 23:47:05 +0400

    'localStorage.getItem('tasks''

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ const taskList = document.querySelector('#tasksList');
 const emptyList = document.querySelector('#emptyList');
 
 let tasks = [];
+
+if (localStorage.getItem('tasks')) {                                                
+	tasks = JSON.parse(localStorage.getItem('tasks'));  
+	tasks.forEach(function (task) {                                                     
+		renderTask(task);
+	});                           
+}
+
 checkEmptyList();                                                                   
 
 form.addEventListener('submit', addTask);                                           
@@ -18,22 +26,9 @@ function addTask(event) {
 		text: taskText,
 		done: false,
 	};
-	tasks.push(newTask);                                                                
-	const cssClass = newTask.done ? 'task-title task-title--done' : 'task-title';        
-    const taskHTML = `                                                               
-    <li id="${newTask.id}" class="list-group-item d-flex justify-content-between task-item">
-					<span class="${cssClass}">${newTask.text}</span>
-					<div class="task-item__buttons">
-						<button type="button" data-action="done" class="btn-action">
-							<img src="./img/tick.svg" alt="Done" width="18" height="18">
-						</button>
-						<button type="button" data-action="delete" class="btn-action">
-							<img src="./img/cross.svg" alt="Done" width="18" height="18">
-						</button>
-					</div>
-				</li>
-                `;                                                                   
-	taskList.insertAdjacentHTML('beforeend', taskHTML);                              
+	tasks.push(newTask);   
+	saveToLocalStorage();                                                             
+	renderTask(newTask);                             
 	taskInput.value = ''                                                             
 	checkEmptyList();                                                                
 }
@@ -43,7 +38,8 @@ function deleteTask(event) {
 		const parentNode = event.target.closest('li');                               
 		const id = Number(parentNode.id);                                            
 		const index = tasks.findIndex((task) => task.id === id);               
-		tasks.splice(index, 1);                                                      
+		tasks.splice(index, 1);   
+		saveToLocalStorage();                                                                                                                
 		parentNode.remove(); 		                                                 
 		checkEmptyList();                                                            
 }
@@ -57,7 +53,8 @@ function doneTask(event) {
 				return true
 			}
 		});    
-		task.done = !task.done                                                       
+		task.done = !task.done;
+		saveToLocalStorage();                                                                                                                    
 		const taskTitle = parentNode.querySelector('span');
 		taskTitle.classList.toggle('task-title--done');                              
 }
@@ -74,5 +71,27 @@ function checkEmptyList() {
 		const emptyListEl = document.querySelector('#emptyList');
 		emptyListEl ? emptyListEl.remove() : null;                                   
 	}
-};
+}
+
+function saveToLocalStorage() {
+	localStorage.setItem('tasks', JSON.stringify(tasks))                             
+}
+
+function renderTask(task) {
+	const cssClass = task.done ? 'task-title task-title--done' : 'task-title';      
+    const taskHTML = `                                                               
+    <li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+					<span class="${cssClass}">${task.text}</span>
+					<div class="task-item__buttons">
+						<button type="button" data-action="done" class="btn-action">
+							<img src="./img/tick.svg" alt="Done" width="18" height="18">
+						</button>
+						<button type="button" data-action="delete" class="btn-action">
+							<img src="./img/cross.svg" alt="Done" width="18" height="18">
+						</button>
+					</div>
+				</li>
+                `;                                                                   
+	taskList.insertAdjacentHTML('beforeend', taskHTML);
+}
 
